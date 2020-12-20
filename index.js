527b094e277a62c34d428daaa4f4bc7078f86213
Chris Neal <chrisvneal@gmail.com>
2020-12-19 14:00:34 -1000

    create store function frmo scratch

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-function createStore() {}
+// create a store
+
+function createStore() {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => {
+        l !== listener;
+      });
+    };
+  };
+
+  return {
+    getState,
+    subscribe,
+  };
+}
