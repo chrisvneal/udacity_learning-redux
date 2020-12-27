4ef9c6d1d0e6312c6b585bb92aaca559f43f790f
Chris Neal <chrisvneal@gmail.com>
2020-12-26 14:32:44 -1000

    Add comments explaining createStore function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Library Code
 function createStore(reducer) {
+  // create state variable
   let state;
   let listeners = [];
 
+  // access the current state
   const getState = () => state;
+
+  // subscribe to state updates
   const subscribe = (listener) => {
     listeners.push(listener);
 
@@ -12,11 +16,13 @@ function createStore(reducer) {
     };
   };
 
+  // update state with  dispatch function
   const dispatch = (action) => {
     state = reducer((state = []), action);
     listeners.forEach((listener) => listener());
   };
 
+  // return all user variables
   return { getState, subscribe, dispatch };
 }
 
@@ -28,17 +34,13 @@ function todos(state = [], action) {
   } else if ((action.type = "REMOVE_TODO")) {
   } else if ((action.type = "TOGGLE_TODO")) {
   }
-
-  const store = createStore(todos);
-
-  store.subscribe(() => {
-    console.log("The new state is: " + store.getState());
-  });
-
-  const unsubscribe = store.subscribe(() => {
-    console.log("The store changed");
-  });
 }
+
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log("The new state is: " + store.getState());
+});
 
 store.dispatch({
   type: "ADD_TODO",
